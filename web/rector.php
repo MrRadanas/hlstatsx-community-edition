b157f64ae22259d9d9d49ffb20a5ad2626d74904
Stanislav Krotov <JIorDMI@gmail.com>
2024-02-07 00:05:59 +0300

    web added cs-fix

---
+++ web/rector.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/includes',
+        __DIR__ . '/pages',
+        __DIR__ . '/updater_1',
+    ])
+    // uncomment to reach your current PHP version
+    // ->withPhpSets()
+    ->withRules([
+        AddVoidReturnTypeWhereNoReturnRector::class,
+    ])
+    ->withSets([
+        LevelSetList::UP_TO_PHP_74
+    ]);
